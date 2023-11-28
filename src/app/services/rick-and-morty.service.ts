5e3bc05e5df481642af4bf69171753617cedddef
Miguel Angel Ramos <return.infox@gmail.com>
2023-11-28 19:30:16 -0300

    zona refactor

--- src/app/services/rick-and-morty.service.ts
+++ src/app/services/rick-and-morty.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
+import { IResponseAPI } from "../interfaces/IResponseApi";
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,11 @@ import { Observable } from "rxjs";
 
 export class RickAndMortyService {
 
-  private apiUrl = 'https://rickandmortyapi.com/api/character/';
+  private readonly API_URL = environment.api;
 
   constructor(private http: HttpClient) {}
 
-  getCharacters():Observable<any> {
-    return this.http.get(this.apiUrl);
+  getCharacters():Observable<IResponseAPI> {
+    return this.http.get<IResponseAPI>(this.API_URL);
   }
 }
